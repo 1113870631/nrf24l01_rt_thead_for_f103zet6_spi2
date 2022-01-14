05f6b8d04cc28d08e474d757f6145fccf22b28bc
1113870631 <1113870631@qq.com>
2022-01-14 13:15:46 +0800

    first

---
+++ applications/main.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-01-13     RT-Thread    first version
+ */
+
+#include <rtthread.h>
+
+#define DBG_TAG "main"
+#define DBG_LVL DBG_LOG
+#include <rtdbg.h>
+
+int main(void)
+{
+
+
+
+        LOG_D("Hello RT-Thread!");
+
+    return RT_EOK;
+}
